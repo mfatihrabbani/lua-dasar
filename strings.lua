8f858ed8fede47a51a889c98121121dc8a03af7f
mfatihrabbani <mfatihrabbani@gmail.com>
2023-06-21 16:40:27 +0700

    update materi strings

--- strings.lua
+++ strings.lua
@@ -5,3 +5,16 @@ print(string.upper(buah))
 print(string.lower(buah))
 
 print(string.len(buah))
+
+namaDepan = "Andi"
+namaBelakang = "Prasetyo"
+
+namaLengkap = namaDepan + namaBelakang
+print(namaLengkap)
+
+namaLengkap = namaDepan..namaBelakang
+
+string = "Nomor"
+angka = 1
+
+gabungan = string..angka
---
+++ table.lua
@@ -0,0 +1,15 @@
+tableKosong = {}
+
+tableArray = {"Ayam", "Gajah", "Sapi", "Kambing"}
+print(tableArray[1])
+
+tableKeyValue = {
+    name = "Andi Angga",
+    age = 20,
+    city = "Jakarta"
+}
+print(tableKeyValue.name)
+
+
+table.insert(tableArray, "Bebek")
+table.remove(tableArray, 1)
